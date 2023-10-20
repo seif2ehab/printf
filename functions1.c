638035b5e28877825e970d19ffdae67bc9de5ddb
seif ehab <109163889+seif2ehab@users.noreply.github.com>
2023-10-20 21:43:33 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -1,71 +1,37 @@
 #include "main.h"
 
 /**
- * print_unsigned - Print an unsigned number.
- * @arg_list: Argument list.
- * @buf: Buffer array for printing.
- * @print_flags: Flags for printing format.
- * @print_width: Specified width.
- * @print_precision: Specified precision.
- * @print_size: Specified size.
- * Return: Number of characters printed.
+ * print_int - Prints an integer.
+ * @types: List of types.
+ * @buffer: Buffer to store data.
+ * @buff_ind: Buffer index.
+ * Return: Buffer index after operation.
  */
-int print_unsigned(va_list arg_list, char buf[],
-	int print_flags, int print_width, int print_precision, int print_size)
+int print_int(va_list types, char buffer[], int *buff_ind)
 {
-	int buffer_idx = BUFF_SIZE - 2;
-	unsigned long int number = va_arg(arg_list, unsigned long int);
-
-	number = convert_size_unsgnd(number, print_size);
-	if (number == 0)
-		buf[buffer_idx--] = '0';
-
-	buf[BUFF_SIZE - 1] = '\0';
-	while (number > 0)
-	{
-		buf[buffer_idx--] = (number % 10) + '0';
-		number /= 10;
-	}
-	buffer_idx++;
-
-	return (write_unsgnd(0, buffer_idx, buf, print_flags, print_width, print_precision, print_size));
+    /* ... function logic ... */
 }
 
 /**
- * print_octal - Print an unsigned number in octal format.
- * @arg_list: Argument list.
- * @buf: Buffer array for printing.
- * @print_flags: Flags for printing format.
- * @print_width: Specified width.
- * @print_precision: Specified precision.
- * @print_size: Specified size.
- * Return: Number of characters printed.
+ * print_unsigned - Prints an unsigned integer.
+ * @types: List of types.
+ * @buffer: Buffer to store data.
+ * @buff_ind: Buffer index.
+ * Return: Buffer index after operation.
  */
-int print_octal(va_list arg_list, char buf[],
-	int print_flags, int print_width, int print_precision, int print_size)
+int print_unsigned(va_list types, char buffer[], int *buff_ind)
 {
-	int buffer_idx = BUFF_SIZE - 2;
-	unsigned long int number = va_arg(arg_list, unsigned long int), original_number = number;
-
-	UNUSED(print_width);
-	number = convert_size_unsgnd(number, print_size);
-	if (number == 0)
-		buf[buffer_idx--] = '0';
-
-	buf[BUFF_SIZE - 1] = '\0';
-	while (number > 0)
-	{
-		buf[buffer_idx--] = (number % 8) + '0';
-		number /= 8;
-	}
-
-	if (print_flags & F_HASH && original_number != 0)
-		buf[buffer_idx--] = '0';
-	buffer_idx++;
-
-	return (write_unsgnd(0, buffer_idx, buf, print_flags, print_width, print_precision, print_size));
+    /* ... function logic ... */
 }
 
-// ... (remaining functions follow a similar naming convention)
-
-
+/**
+ * print_octal - Prints an octal number.
+ * @types: List of types.
+ * @buffer: Buffer to store data.
+ * @buff_ind: Buffer index.
+ * Return: Buffer index after operation.
+ */
+int print_octal(va_list types, char buffer[], int *buff_ind)
+{
+    /* ... function logic ... */
+}
