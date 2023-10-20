04f89efeadfb704c844981fdddc0e852d63ce338
seif ehab <109163889+seif2ehab@users.noreply.github.com>
2023-10-20 16:05:38 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,77 +1,65 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
 /**
- * custom_printf - Custom implementation of printf.
- * @input_format: The format string.
- *
- * Return: Number of characters printed.
+ * output_buffer - Outputs the buffer's content to the standard output.
+ * @buf: Buffer containing characters.
+ * @index: Current position in the buffer.
  */
-int custom_printf(const char *input_format, ...);
-
-/**
- * main - Main function to test custom_printf.
- *
- * Return: Always 0.
- */
-int main(void)
+void output_buffer(char *buf, int *index)
 {
-    custom_printf("Hello, %corld! %s\n", 'W', "This is a test.");
-    return (0);
+	if (*index > 0)
+		write(1, buf, *index);
+
+	*index = 0;
 }
 
 /**
- * custom_printf - Custom implementation of printf.
- * @input_format: The format string.
+ * custom_printf - A custom implementation of the printf function.
+ * @pattern: The format string.
  *
  * Return: Number of characters printed.
  */
-int custom_printf(const char *input_format, ...)
+int custom_printf(const char *pattern, ...)
 {
-    va_list arg_list;
-    int chars_printed = 0;
+	int pos, char_count = 0, temp_count = 0;
+	int formatting_flags, field_width, num_precision, specifier_size;
+	int buffer_pos = 0;
+	va_list args;
+	char temp_buffer[BUFF_SIZE];
 
-    va_start(arg_list, input_format);
-    while (*input_format)
-    {
-        if (*input_format == '%')
-        {
-            input_format++;
-            switch (*input_format)
-            {
-                case 'c':
-                    putchar(va_arg(arg_list, int));
-                    chars_printed++;
-                    break;
-                case 's':
-                {
-                    char *str = va_arg(arg_list, char *);
-                    while (*str)
-                    {
-                        putchar(*str);
-                        chars_printed++;
-                        str++;
-                    }
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    chars_printed++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*input_format);
-                    chars_printed += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*input_format);
-            chars_printed++;
-        }
-        input_format++;
-    }
-    va_end(arg_list);
-    return (chars_printed);
+	if (!pattern)
+		return (-1);
+
+	va_start(args, pattern);
+
+	for (pos = 0; pattern[pos]; pos++)
+	{
+		if (pattern[pos] != '%')
+		{
+			temp_buffer[buffer_pos++] = pattern[pos];
+			if (buffer_pos == BUFF_SIZE)
+				output_buffer(temp_buffer, &buffer_pos);
+			char_count++;
+		}
+		else
+		{
+			output_buffer(temp_buffer, &buffer_pos);
+			formatting_flags = fetch_flags(pattern, &pos);
+			field_width = fetch_width(pattern, &pos, args);
+			num_precision = fetch_precision(pattern, &pos, args);
+			specifier_size = fetch_size(pattern, &pos);
+			pos++;
+			temp_count = manage_formatting(pattern, &pos, args, temp_buffer,
+				formatting_flags, field_width, num_precision, specifier_size);
+			if (temp_count == -1)
+				return (-1);
+			char_count += temp_count;
+		}
+	}
+
+	output_buffer(temp_buffer, &buffer_pos);
+
+	va_end(args);
+
+	return (char_count);
 }
