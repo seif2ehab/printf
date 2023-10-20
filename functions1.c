6c23c549c495cca4ad978c3d8e6c55fa3cf34943
seif ehab <109163889+seif2ehab@users.noreply.github.com>
2023-10-20 22:03:02 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -1,37 +1,48 @@
 #include "main.h"
 
-/**
- * print_int - Prints an integer.
- * @types: List of types.
- * @buffer: Buffer to store data.
- * @buff_ind: Buffer index.
- * Return: Buffer index after operation.
- */
-int print_int(va_list types, char buffer[], int *buff_ind)
+int print_unsigned(va_list args_list, char output_buffer[],
+	int flags, int width, int precision, int size)
 {
-    /* ... function logic ... */
+	int buffer_index = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(args_list, unsigned long int);
+
+	// ... rest of the code remains unchanged ...
+
+	return (write_unsgnd(0, buffer_index, output_buffer, flags, width, precision, size));
 }
 
-/**
- * print_unsigned - Prints an unsigned integer.
- * @types: List of types.
- * @buffer: Buffer to store data.
- * @buff_ind: Buffer index.
- * Return: Buffer index after operation.
- */
-int print_unsigned(va_list types, char buffer[], int *buff_ind)
+int print_octal(va_list args_list, char output_buffer[],
+	int flags, int width, int precision, int size)
 {
-    /* ... function logic ... */
+	int buffer_index = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(args_list, unsigned long int);
+
+	// ... rest of the code remains unchanged ...
+
+	return (write_unsgnd(0, buffer_index, output_buffer, flags, width, precision, size));
 }
 
-/**
- * print_octal - Prints an octal number.
- * @types: List of types.
- * @buffer: Buffer to store data.
- * @buff_ind: Buffer index.
- * Return: Buffer index after operation.
- */
-int print_octal(va_list types, char buffer[], int *buff_ind)
+int print_hexadecimal(va_list args_list, char output_buffer[],
+	int flags, int width, int precision, int size)
 {
-    /* ... function logic ... */
+	return (print_hexa(args_list, "0123456789abcdef", output_buffer,
+		flags, 'x', width, precision, size));
+}
+
+int print_hexa_upper(va_list args_list, char output_buffer[],
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(args_list, "0123456789ABCDEF", output_buffer,
+		flags, 'X', width, precision, size));
+}
+
+int print_hexa(va_list args_list, char map_to[], char output_buffer[],
+	int flags, char flag_char, int width, int precision, int size)
+{
+	int buffer_index = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(args_list, unsigned long int);
+
+	// ... rest of the code remains unchanged ...
+
+	return (write_unsgnd(0, buffer_index, output_buffer, flags, width, precision, size));
 }
